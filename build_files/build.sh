e7ca8cd6b45b31dc410f6deee33f1812f8b2cda8
D <dector@dector.space>
2025-11-04 18:48:41 +0100

    feat: just install mise, don't install mise apps

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,22 +9,13 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y \
-  docker docker-compose
+# Install docker
+dnf install -y docker docker-compose
 
 # Install mise from COPR
-dnf5 -y copr enable jdxcode/mise \
-  && dnf5 -y install mise \
-  && mise i \
-    bun deno node java java@17 uv zig ubi:leoafarias/fvm \
-    jj ubi:idursun/jjui lazygit ubi:afnanenayet/diffsitter \
-    bat tmux zellij starship ubi:nushell/nushell \
-    claude npm:@github/copilot ubi:block/goose \
-    ubi:dector/bang ubi:dector/serv \
-    ubi:dector/lampa \
-    ubi:tailwindlabs/tailwindcss \
-  && dnf5 -y copr disable jdxcode/mise # Disable COPRs so they don't end up enabled on the final image:
+dnf -y copr enable jdxcode/mise \
+  && dnf -y install mise
+  #&& dnf5 -y copr disable jdxcode/mise # Disable COPRs so they don't end up enabled on the final image:
 
 ### Enabling a System Unit File
 
@@ -32,5 +23,5 @@ systemctl enable podman.socket
 
 ### Cleanup
 
-dnf5 clean all
+dnf clean all
 
